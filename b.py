c5f6bd47b81b58ea655ae77d9808054fe105f897
Mayank Sinha <mayanksinha222000@gmail.com>
2023-03-27 17:01:39 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,17 @@
+# to find greatest among two numbers
+a =76
+b =89
+if a>b:
+    print("The greater no is :" + str(a))
+else:
+    print("The greater no is :" + str(b))    
+
+
+
+
+# a = int(input("enter a\n"))
+# b = int(input("enter b\n"))
+
+# # if a>b:
+# #  print("A B se bada hai bhai")
+# print("B A se bada hai bhai") if a<b else print("A B se bara h yrr")
